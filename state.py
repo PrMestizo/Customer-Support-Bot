1e0b551f219960cfed97bb126317c98ea8576cad
Andreu Llopis <130099847+PrMestizo@users.noreply.github.com>
2024-07-07 09:47:34 +0200

    Update state.py

--- state.py
+++ state.py
@@ -1,7 +1,85 @@
 from typing import Annotated
+from datetime import datetime
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
 from typing_extensions import TypedDict
 from langgraph.graph.message import AnyMessage, add_messages
-
+from Tools.hotels import book_hotel, search_hotels, update_hotel, cancel_hotel
+from Tools.flights import update_ticket_to_new_flight, search_flights, fetch_user_flight_information, cancel_ticket
+from Tools.reservations import  search_trip_recommendations, book_excursion, update_excursion, cancel_excursion
+from Tools.carRental import search_car_rentals, book_car_rental, update_car_rental, cancel_car_rental
+from Tools.policy import lookup_policy
 
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
+    user_info: str
+
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            result = self.runnable.invoke(state)
+            # If the LLM happens to return an empty response, we will re-prompt it
+            # for an actual response.
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
+
+# Haiku is faster and cheaper, but less accurate
+# llm = ChatAnthropic(model="claude-3-haiku-20240307")
+llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=1)
+# You could also use OpenAI or another model, though you will likely have
+# to adapt the prompts
+# from langchain_openai import ChatOpenAI
+
+# llm = ChatOpenAI(model="gpt-4-turbo-preview")
+
+assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n\n{user_info}\n"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now())
+
+part_2_tools = [
+    TavilySearchResults(max_results=1),
+    fetch_user_flight_information,
+    search_flights,
+    lookup_policy,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+]
+part_2_assistant_runnable = assistant_prompt | llm.bind_tools(part_2_tools)
