fd64feae1ae791d6b97419c704a5c85fbd9c38b0
Andreu Llopis <130099847+PrMestizo@users.noreply.github.com>
2024-07-07 11:57:38 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,13 +2,14 @@ from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph import END, StateGraph, START
 from langgraph.prebuilt import tools_condition
 from state import State
-from agent import Assistant, part_2_assistant_runnable, part_2_tools
+from agent import Assistant, part_3_assistant_runnable, part_3_safe_tools, part_3_sensitive_tools, sensitive_tool_names
 from helperFunctions import create_tool_node_with_fallback
 import shutil
 import uuid
 from helperFunctions import _print_event
 from Tools.flights import fetch_user_flight_information
 from langchain_core.messages import ToolMessage
+from typing import Literal
 
 
 backup_file = "travel2.backup.sqlite"
@@ -23,18 +24,38 @@ def user_info(state: State):
 # having to take an action
 builder.add_node("fetch_user_info", user_info)
 builder.add_edge(START, "fetch_user_info")
-builder.add_node("assistant", Assistant(part_2_assistant_runnable))
-builder.add_node("tools", create_tool_node_with_fallback(part_2_tools))
+builder.add_node("assistant", Assistant(part_3_assistant_runnable))
+builder.add_node("safe_tools", create_tool_node_with_fallback(part_3_safe_tools))
+builder.add_node(
+    "sensitive_tools", create_tool_node_with_fallback(part_3_sensitive_tools)
+)
+# Define logic
 builder.add_edge("fetch_user_info", "assistant")
+
+
+def route_tools(state: State) -> Literal["safe_tools", "sensitive_tools", "__end__"]:
+    next_node = tools_condition(state)
+    # If no tools are invoked, return to the user
+    if next_node == END:
+        return END
+    ai_message = state["messages"][-1]
+    # This assumes single tool calls. To handle parallel tool calling, you'd want to
+    # use an ANY condition
+    first_tool_call = ai_message.tool_calls[0]
+    if first_tool_call["name"] in sensitive_tool_names:
+        return "sensitive_tools"
+    return "safe_tools"
+
+
 builder.add_conditional_edges(
     "assistant",
-    tools_condition,
+    route_tools,
 )
-builder.add_edge("tools", "assistant")
-
+builder.add_edge("safe_tools", "assistant")
+builder.add_edge("sensitive_tools", "assistant")
 
 memory = SqliteSaver.from_conn_string(":memory:")
-part_2_graph = builder.compile(
+part_3_graph = builder.compile(
     checkpointer=memory,
     # NEW: The graph will always halt before executing the "tools" node.
     # The user can approve or reject (or even alter the request) before
@@ -78,12 +99,12 @@ tutorial_questions = [
 _printed = set()
 # We can reuse the tutorial questions from part 1 to see how it does.
 for question in tutorial_questions:
-    events = part_2_graph.stream(
+    events = part_3_graph.stream(
         {"messages": ("user", question)}, config, stream_mode="values"
     )
     for event in events:
         _print_event(event, _printed)
-    snapshot = part_2_graph.get_state(config)
+    snapshot = part_3_graph.get_state(config)
     while snapshot.next:
         # We have an interrupt! The agent is trying to use a tool, and the user can approve or deny it
         # Note: This code is all outside of your graph. Typically, you would stream the output to a UI.
@@ -94,14 +115,14 @@ for question in tutorial_questions:
         )
         if user_input.strip() == "y":
             # Just continue
-            result = part_2_graph.invoke(
+            result = part_3_graph.invoke(
                 None,
                 config,
             )
         else:
             # Satisfy the tool invocation by
             # providing instructions on the requested changes / change of mind
-            result = part_2_graph.invoke(
+            result = part_3_graph.invoke(
                 {
                     "messages": [
                         ToolMessage(
@@ -112,4 +133,4 @@ for question in tutorial_questions:
                 },
                 config,
             )
-        snapshot = part_2_graph.get_state(config)
+        snapshot = part_3_graph.get_state(config)
